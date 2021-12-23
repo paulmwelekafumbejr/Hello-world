47a55b29ecfa6d49718f6c44402345ffbca6d4f2
Paulgazza2008 <paulkafumbe@gmail.com>
2021-12-23 19:51:13 +0300

    Initial commit

---
+++ src/geekbrains/lesson_1/Main.java
@@ -0,0 +1,9 @@
+package geekbrains.lesson_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+	// write your code here
+    }
+}
